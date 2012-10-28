5cfb76ea7586e3072473e88e482416dd7f52a8f7
damoun <damoun60@gmail.com>
2012-10-28 22:12:18 +0800

    update example

--- examples/server.js
+++ examples/server.js
@@ -1,24 +1,38 @@
 #!/usr/bin/env node
 
-var dnsserver = require('../lib/dnsserver');
+var util = require('util'),
+dnsserver = require('../lib/dnsserver'),
+AR = require('../lib/AR'),
+MXR = require('../lib/MXR'),
+NSR = require('../lib/NSR');
 
 var server = dnsserver.createServer();
 server.bind(8000, '127.0.0.1');
 
+var nameList = [];
+nameList.push(new AR('tomhughescroucher.com.', '184.106.231.91'));
+nameList.push(new AR('tomhughescroucher.com.', '127.0.0.1'));
+nameList.push(new NSR('tomhughescroucher.com.', 'eforward3.registrar-servers.com.'));
+
 server.on('request', function(req, res) {
-  console.log("req = ", req);
-  var question = req.question;
+    // console.log("req = ", req);
 
-  if (question.type == 1 && question.class == 1 && question.name == 'tomhughescroucher.com') {
-    // IN A query
-    res.addRR(question.name, 1, 1, 3600, '184.106.231.91');
-  } else {
-    res.header.rcode = 3; // NXDOMAIN
-  }
+    for (var i = 0; i < nameList.length; ++i) {
+	if (nameList[i].getClass() == req.question.qclass
+	    && nameList[i].getType() == req.question.qtype
+	    && nameList[i].getName().toString() == req.question.qname.toString()) {
+	    res.addRR(nameList[i].getName(),
+		      nameList[i].getClass(),
+		      nameList[i].getType(),
+		      nameList[i].getTtl(),
+		      nameList[i].getRdlength(),
+		      nameList[i].getRdata());
+	}
+    }
 
-  res.send();
+    res.send();
 });
 
 server.on('error', function(e) {
-  throw e;
+    throw e;
 });
