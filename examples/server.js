4cd54841f162650ad9944c5dba050ec33fa532ea
damoun <damoun60@gmail.com>
2012-10-30 00:20:09 +0800

    added some record example

--- examples/server.js
+++ examples/server.js
@@ -4,29 +4,46 @@ var util = require('util'),
 dnsserver = require('../lib/dnsserver'),
 AR = require('../lib/AR'),
 MXR = require('../lib/MXR'),
-NSR = require('../lib/NSR');
+NSR = require('../lib/NSR'),
+SOAR = require('../lib/SOAR');
 
 var server = dnsserver.createServer();
 server.bind(8000, '127.0.0.1');
 
 var nameList = [];
-nameList.push(new AR('tomhughescroucher.com.', '184.106.231.91'));
-nameList.push(new AR('tomhughescroucher.com.', '127.0.0.1'));
-nameList.push(new NSR('tomhughescroucher.com.', 'eforward3.registrar-servers.com.'));
+nameList.push(new AR('google.com.', 1800, '173.194.34.0'));
+nameList.push(new AR('google.com.', 1800, '173.194.34.6'));
+nameList.push(new AR('google.com.', 1800, '173.194.34.3'));
+nameList.push(new AR('google.com.', 1800, '173.194.34.5'));
+nameList.push(new AR('google.com.', 1800, '173.194.34.8'));
+nameList.push(new AR('google.com.', 1800, '173.194.34.2'));
+nameList.push(new AR('google.com.', 1800, '173.194.34.9'));
+nameList.push(new AR('google.com.', 1800, '173.194.34.14'));
+nameList.push(new AR('google.com.', 1800, '173.194.34.1'));
+nameList.push(new AR('google.com.', 1800, '173.194.34.7'));
+nameList.push(new AR('google.com.', 1800, '173.194.34.4'));
+
+nameList.push(new NSR('google.com.', 1800, 'ns1.google.com.'));
+nameList.push(new NSR('google.com.', 1800, 'ns2.google.com.'));
+nameList.push(new NSR('google.com.', 1800, 'ns3.google.com.'));
+nameList.push(new NSR('google.com.', 1800, 'ns4.google.com.'));
+
+nameList.push(new MXR('google.com.', 1800, 10, 'aspmx.l.google.com.'));
+nameList.push(new MXR('google.com.', 1800, 20, 'alt1.aspmx.l.google.com.'));
+nameList.push(new MXR('google.com.', 1800, 30, 'alt2.aspmx.l.google.com.'));
+nameList.push(new MXR('google.com.', 1800, 40, 'alt3.aspmx.l.google.com.'));
+nameList.push(new MXR('google.com.', 1800, 50, 'alt4.aspmx.l.google.com.'));
+
+nameList.push(new SOAR('google.com.', 1800, 'ns1.google.com.', 'dns-admin.google.com.', 2012101500, 7200, 1800, 1209600, 300));
 
 server.on('request', function(req, res) {
-    // console.log("req = ", req);
+    console.log("req = ", req);
 
     for (var i = 0; i < nameList.length; ++i) {
 	if (nameList[i].getClass() == req.question.qclass
 	    && nameList[i].getType() == req.question.qtype
 	    && nameList[i].getName().toString() == req.question.qname.toString()) {
-	    res.addRR(nameList[i].getName(),
-		      nameList[i].getClass(),
-		      nameList[i].getType(),
-		      nameList[i].getTtl(),
-		      nameList[i].getRdlength(),
-		      nameList[i].getRdata());
+	    res.addRR(nameList[i]);
 	}
     }
 
